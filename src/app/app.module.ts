4254ab48c6a25ac425050a6b4b4f870931b2bbdb
looegi <lc1512@humboldt.edu>
2023-01-30 14:04:25 -0800

    scully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +16,8 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     CoreModule,
     ContactModule,
-    SharedModule
+    SharedModule,
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
