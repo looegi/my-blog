1a9d94edc5b85dd4d2db9274e452d65d0391f700
looegi <lc1512@humboldt.edu>
2023-01-29 19:16:08 -0800

    added contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ContactModule } from './contact/contact.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +14,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    ContactModule,
     SharedModule
   ],
   providers: [],
